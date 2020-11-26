9b8ff16d1a27a189db1e3c8271d325544cf5f78d
PS <ps@booghi.com>
2020-11-26 16:57:42 +0330

    added evaluation

--- Classification.py
+++ Classification.py
@@ -48,14 +48,19 @@ def main(FLAGS):
     dataset_dir = FLAGS.data_dir
     batch_size=FLAGS.batch_size
     epochs=FLAGS.epochs
+    mode=FLAGS.mode
     train_dir = os.path.join(dataset_dir, 'train')
     validation_dir = os.path.join(dataset_dir, 'validate')
+    test_dir = os.path.join(dataset_dir, 'test')
     classes= os.listdir(train_dir)
+
 
     image_generator = ImageDataGenerator(rescale=1/255.)
     train_gn = image_generator.flow_from_directory(directory=train_dir, batch_size=batch_size, shuffle=True,
                     target_size=(120, 220), classes=classes)
     validation_gn = image_generator.flow_from_directory(directory=validation_dir, batch_size=batch_size, shuffle=True,
+                    target_size=(120, 220), classes=classes)
+    test_gn = image_generator.flow_from_directory(directory=test_dir, batch_size=batch_size, shuffle=True,
                     target_size=(120, 220), classes=classes)
 
 
@@ -63,7 +68,8 @@ def main(FLAGS):
     model.compile(optimizer= 'adam', loss=CategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
     model.summary()
 
-    model.fit(train_gn, epochs=epochs, validation_data=validation_gn)
+    history = model.fit(train_gn, epochs=epochs, validation_data=validation_gn)
+    results = model.evaluate(test_gn, verbose=True)
 
 
 if __name__ == '__main__':
@@ -75,7 +81,7 @@ if __name__ == '__main__':
                       default=128)
     parser.add_argument('--epochs', type=int,
                     default=50)
-
+    parser.add_argument('-mode', default='train', type=str)
 
     FLAGS, unparsed = parser.parse_known_args()
     main(FLAGS)
